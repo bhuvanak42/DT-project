177c66dbf018e4f1f5537242ecf39e2afa7a8ccd
bhuvanak42 <bhuvanak42@yahoo.in>
2016-08-11 14:35:09 +0530

    Shopping cart backend
    category, supplier dao interface and implementation.....

---
+++ ShoppingCartBackend/src/main/java/com/niit/shoppingcartbackend/dao/SupplierDAO.java
@@ -0,0 +1,11 @@
+package com.niit.shoppingcartbackend.dao;
+import java.util.List;
+import com.niit.shoppingcartbackend.model.Supplier;
+
+public interface SupplierDAO {
+	public boolean save(Supplier supplier);
+	public boolean delete(Supplier supplier);
+	public boolean update(Supplier supplier);
+	public Supplier get(String id);
+	public List<Supplier> list();
+}
